b7224e10706042d3e1430dbf7f8e65419227eb43
xreynihbos <csrynaldifusototo@gmail.com>
2025-11-20 16:05:29 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,40 +1,19 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+import os
+from openai import OpenAI
 
-const HF_TOKEN = "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"
-const MODEL = "meta-llama/Llama-3.2-3B-Instruct:novita"
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.environ["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"],
+)
 
-async function handleRequest(request) {
-  if (request.method === "POST") {
-    try {
-      const body = await request.json();
+completion = client.chat.completions.create(
+    model="meta-llama/Llama-3.2-3B-Instruct:novita",
+    messages=[
+        {
+            "role": "user",
+            "content": "What is the capital of France?"
+        }
+    ],
+)
 
-      const hfResponse = await fetch("https://router.huggingface.co/v1/chat/completions", {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${HF_TOKEN}`,
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          model: MODEL,
-          messages: body.messages // terima messages dari frontend
-        })
-      });
-
-      const data = await hfResponse.json();
-
-      return new Response(JSON.stringify(data), {
-        headers: { "Content-Type": "application/json" }
-      });
-
-    } catch (err) {
-      return new Response(JSON.stringify({ error: err.message }), {
-        status: 500,
-        headers: { "Content-Type": "application/json" }
-      });
-    }
-  } else {
-    return new Response("Cloudflare Worker AI Proxy", { status: 200 });
-  }
-}
+print(completion.choices[0].message)
